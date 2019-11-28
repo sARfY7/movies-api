95e3cbfbbc9879ca30b6ab55536fb58cdf23aad0
Sarfaraz Ahmed <imsarfy@gmail.com>
2019-11-28 20:10:43 +0530

    sequelize support added

--- controllers/director.controller.js
+++ controllers/director.controller.js
@@ -1,60 +1,60 @@
-const Director = require('../models/director.model');
+const { Director } = require('../models');
 
 const { log } = console;
 
 const createDirector = (req, res) => {
   const { directorName } = req.body;
-  Director.create(directorName, (err, directorCreated) => {
-    if (err) {
-      log(err);
-    } else {
+  Director.create({ name: directorName })
+    .then((directorCreated) => {
       res.status(201).json(directorCreated);
-    }
-  });
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
 };
 
 const readDirector = (req, res) => {
   const { id } = req.params;
-  Director.readById(id, (err, director) => {
-    if (err) {
-      log(err);
-    } else {
+  Director.findByPk(id)
+    .then((director) => {
       res.status(200).json(director);
-    }
-  });
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
 };
 
 const readAllDirectors = (req, res) => {
-  Director.read((err, directors) => {
-    if (err) {
-      log(err);
-    } else {
+  Director.findAll()
+    .then((directors) => {
       res.status(200).json(directors);
-    }
-  });
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
 };
 
 const updateDirector = (req, res) => {
   const { id } = req.params;
   const { newDirectorName } = req.body;
-  Director.updateById(id, newDirectorName, (err, updatedDirector) => {
-    if (err) {
-      log(err);
-    } else {
+  Director.update({ name: newDirectorName }, { returning: true, where: { id } })
+    .then((updatedDirector) => {
       res.status(200).json(updatedDirector);
-    }
-  });
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
 };
 
 const deleteDirector = (req, res) => {
   const { id } = req.params;
-  Director.deleteById(id, (err, deletedDirector) => {
-    if (err) {
-      log(err);
-    } else {
+  Director.destroy({ where: { id } })
+    .then((deletedDirector) => {
       res.status(200).json(deletedDirector);
-    }
-  });
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
 };
 
 module.exports = {
--- controllers/movie.controller.js
+++ controllers/movie.controller.js
@@ -1,4 +1,4 @@
-const Movie = require('../models/movie.model');
+const { Movie } = require('../models');
 
 const { log } = console;
 
@@ -17,46 +17,46 @@ const createMovie = (req, res) => {
     movieYear,
   } = req.body;
   const movie = {
-    Title: movieTitle,
-    Description: movieDescription,
-    Runtime: movieRuntime,
-    Genre: movieGenre,
-    Rating: movieRating,
-    Metascore: movieMetascore,
-    Votes: movieVotes,
-    Gross_Earning_in_Mil: movieEarning,
-    Director_Id: movieDirectorId,
-    Actor: movieActor,
-    Year: movieYear,
+    title: movieTitle,
+    description: movieDescription,
+    runtime: movieRuntime,
+    genre: movieGenre,
+    rating: movieRating,
+    metascore: movieMetascore,
+    votes: movieVotes,
+    earning: movieEarning,
+    directorId: movieDirectorId,
+    actor: movieActor,
+    year: movieYear,
   };
-  Movie.create(movie, (err, movieCreated) => {
-    if (err) {
-      log(err);
-    } else {
-      res.status(201).json(movieCreated);
-    }
-  });
+  Movie.create(movie)
+    .then((createdMovie) => {
+      res.status(201).json(createdMovie);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
 };
 
 const readMovie = (req, res) => {
   const { id } = req.params;
-  Movie.readById(id, (err, movie) => {
-    if (err) {
-      log(err);
-    } else {
+  Movie.findByPk(id)
+    .then((movie) => {
       res.status(200).json(movie);
-    }
-  });
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
 };
 
 const readAllMovies = (req, res) => {
-  Movie.read((err, movies) => {
-    if (err) {
-      log(err);
-    } else {
+  Movie.findAll()
+    .then((movies) => {
       res.status(200).json(movies);
-    }
-  });
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
 };
 
 const updateMovie = (req, res) => {
@@ -75,36 +75,36 @@ const updateMovie = (req, res) => {
     newMovieYear,
   } = req.body;
   const newMovie = {
-    Title: newMovieTitle,
-    Description: newMovieDescription,
-    Runtime: newMovieRuntime,
-    Genre: newMovieGenre,
-    Rating: newMovieRating,
-    Metascore: newMovieMetascore,
-    Votes: newMovieVotes,
-    Gross_Earning_in_Mil: newMovieEarning,
-    Director_Id: newMovieDirectorId,
-    Actor: newMovieActor,
-    Year: newMovieYear,
+    title: newMovieTitle,
+    description: newMovieDescription,
+    runtime: newMovieRuntime,
+    genre: newMovieGenre,
+    rating: newMovieRating,
+    metascore: newMovieMetascore,
+    votes: newMovieVotes,
+    earning: newMovieEarning,
+    directorId: newMovieDirectorId,
+    actor: newMovieActor,
+    year: newMovieYear,
   };
-  Movie.updateById(id, newMovie, (err, updatedMovie) => {
-    if (err) {
-      log(err);
-    } else {
+  Movie.update(newMovie, { returning: true, where: { id } })
+    .then((updatedMovie) => {
       res.status(200).json(updatedMovie);
-    }
-  });
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
 };
 
 const deleteMovie = (req, res) => {
   const { id } = req.params;
-  Movie.deleteById(id, (err, deletedMovie) => {
-    if (err) {
-      log(err);
-    } else {
+  Movie.destroy({ where: { id } })
+    .then((deletedMovie) => {
       res.status(200).json(deletedMovie);
-    }
-  });
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
 };
 
 module.exports = {
