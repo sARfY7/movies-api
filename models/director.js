fd01534b69840abde77ddd293aaeef09a6ee9f68
Sarfaraz Ahmed <imsarfy@gmail.com>
2019-11-29 12:50:45 +0530

    Validation added

--- models/director.js
+++ models/director.js
@@ -3,11 +3,13 @@ module.exports = (sequelize, DataTypes) => {
   const Director = sequelize.define(
     'Director',
     {
-      name: DataTypes.STRING,
-      validate: {
-        isAlpha: true,
-        notNull: true,
-        notEmpty: true,
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: { msg: 'Name cannot be null' },
+          notEmpty: { msg: 'Name cannot be empty' },
+        },
       },
     },
     {}
--- models/movie.js
+++ models/movie.js
@@ -5,59 +5,94 @@ module.exports = (sequelize, DataTypes) => {
     {
       title: {
         type: DataTypes.STRING,
-        validate: { notNull: true, notEmpty: true },
+        allowNull: false,
+        validate: {
+          notNull: { msg: 'Title cannot be null' },
+          notEmpty: { msg: 'Title cannot be empty' },
+        },
       },
       description: {
         type: DataTypes.TEXT,
-        validate: { notNull: true, notEmpty: true },
+        allowNull: false,
+        validate: {
+          notNull: { msg: 'Description cannot be null' },
+          notEmpty: { msg: 'Description cannot be empty' },
+        },
       },
       runtime: {
         type: DataTypes.INTEGER,
+        allowNull: false,
         validate: {
-          notNull: true,
-          notEmpty: true,
-          isInt: true,
+          notNull: { msg: 'Runtime cannot be null' },
+          notEmpty: { msg: 'Runtime cannot be empty' },
+          isInt: { msg: 'Runtime should be a valid integer' },
         },
       },
       genre: {
         type: DataTypes.STRING,
-        validate: { notNull: true, notEmpty: true },
+        allowNull: false,
+        validate: {
+          notNull: { msg: 'Genre cannot be null' },
+          notEmpty: { msg: 'Genre cannot be empty' },
+        },
       },
-      rating: DataTypes.FLOAT,
+      rating: {
+        type: DataTypes.FLOAT,
+        allowNull: false,
+        validate: {
+          notNull: { msg: 'Rating cannot be null' },
+          notEmpty: { msg: 'Rating cannot be empty' },
+        },
+      },
       metascore: {
         type: DataTypes.STRING,
-        validate: { notNull: true, notEmpty: true },
+        allowNull: false,
+        validate: {
+          notNull: { msg: 'Metascore cannot be null' },
+          notEmpty: { msg: 'Metascore cannot be empty' },
+        },
       },
       votes: {
         type: DataTypes.INTEGER,
+        allowNull: false,
         validate: {
-          notNull: true,
-          notEmpty: true,
-          isInt: true,
+          notNull: { msg: 'Votes cannot be null' },
+          notEmpty: { msg: 'Votes cannot be empty' },
+          isInt: { msg: 'Votes should be a valid integer' },
         },
       },
       earning: {
         type: DataTypes.STRING,
-        validate: { notNull: true, notEmpty: true },
+        allowNull: false,
+        validate: {
+          notNull: { msg: 'Earning cannot be null' },
+          notEmpty: { msg: 'Earning cannot be empty' },
+        },
       },
       actor: {
         type: DataTypes.STRING,
-        validate: { notNull: true, notEmpty: true },
+        allowNull: false,
+        validate: {
+          notNull: { msg: 'Actor cannot be null' },
+          notEmpty: { msg: 'Actor cannot be empty' },
+        },
       },
       year: {
         type: DataTypes.INTEGER,
+        allowNull: false,
         validate: {
-          notNull: true,
-          notEmpty: true,
-          isInt: true,
+          notNull: { msg: 'Year cannot be null' },
+          notEmpty: { msg: 'Year cannot be empty' },
+          isInt: { msg: 'Year should be a valid integer' },
         },
       },
       directorId: {
         type: DataTypes.INTEGER,
+        allowNull: false,
         validate: {
-          notNull: true,
-          notEmpty: true,
-          isInt: true,
+          notNull: { msg: 'Director ID cannot be null' },
+          notEmpty: { msg: 'Director ID cannot be empty' },
+          isInt: { msg: 'Director ID should be a valid integer' },
         },
       },
     },
